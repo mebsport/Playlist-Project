f1380ccfa24573eecacfecd46a7688164d13882e
Marcelus (WHS) <mebsport@gmail.com>
2022-02-07 08:03:40 -0500

    Constructors and variables for song class

--- Song.java
+++ Song.java
@@ -4,5 +4,23 @@
  */
 public class Song
 {
+    private String songName;
+    private String artist;
+    private int length;
+    private String genre;
     
+    public Song()
+    {
+        songName = "";
+        artist = "";
+        length = 0;
+        genre = "";
+    }
+    public Song(String n, String a, int l, String g)
+    {
+        songName = n;
+        artist = a;
+        length = l;
+        genre = g;
+    }
 }
