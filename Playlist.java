fd0263ae427b21a57bdd0d81867c78081a05cd3f
Marcelus (WHS) <mebsport@gmail.com>
2022-02-07 08:31:10 -0500

    constructorsssss

--- Playlist.java
+++ Playlist.java
@@ -8,4 +8,19 @@ public class Playlist
     private ArrayList<Song> playlist = new ArrayList<Song>();
     private String listName;
     private Song currentSong;
+    
+    public Playlist()
+    {
+        listName = "New Playlist";
+    }
+    public Playlist(String name)
+    {
+        listName = name;
+    }
+    public Playlist(String name,Song firstSong)
+    {
+        listName = name;
+        playlist.add(firstSong);
+    }
+    
 }
