398fbdd233bc8fbc04fd9894588791784ede6a96
Marcelus (WHS) <mebsport@gmail.com>
2022-02-07 08:45:45 -0500

    blank methods
    created blank methods in song class to write later on

--- Playlist.java
+++ Playlist.java
@@ -8,6 +8,7 @@ public class Playlist
     private ArrayList<Song> playlist = new ArrayList<Song>();
     private String listName;
     private Song currentSong;
+    private int currentSongIndex;
     
     public Playlist()
     {
@@ -23,4 +24,53 @@ public class Playlist
         playlist.add(firstSong);
     }
     
+    public void addSong(Song song)
+    {
+        playlist.add(song);
+    }
+    public void removeSong()
+    {
+        
+    }
+    public void shuffle(){
+        
+    }
+    public String toString(){
+        String details = "";
+        //return playlist name + number or songs + length
+        return details;
+    }
+    public void updateCurrent(int songIndex){
+        currentSong = playlist.get(songIndex);
+        currentSongIndex = songIndex;
+    }
+    public String getCurrentSongInfo()
+    {
+        return currentSong.toString();        
+    }
+    public String getNextSongInfo()
+    {
+        return playlist.get(currentSongIndex +1).toString(); 
+    }
+    public Song findFromGenre()
+    {
+    
+    }
+    public Song findFromArtist()
+    {
+    
+    }
+    public int getNumSongs()
+    {
+        
+    }
+    public int getLength()
+    {
+        
+    }
+    public void sort()
+    {
+        
+    }
 }
+
